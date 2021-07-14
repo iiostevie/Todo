23dbf66f5a2d87b986f44781f542b516a849e9d7
Poyu <l.poyu1999@gmail.com>
2021-07-14 11:36:57 +0800

    Update user register pages layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>
             Week1 Practice –– Task List
         </title>
 
         <!-- CSS And JavaScript -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+        <!-- Styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
         <style>
-            .container {
-                max-width: 300px;
+            body {
+                font-family: 'Lato';
+            }
+            . fa-btn {
+                margin-right: 6px;
             }
         </style>
+
     </head>
 
 
-    <body>
-        @show
+    <body id="app-layout">
+    <nav class="navbar navbar-default">
         <div class="container">
-            <nav class="navbar navbar-default">
-                <!-- Navbar Contents -->
-            </nav>
+            <div class="navbar-header">
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Task List
+                </a>
+            </div>
+
         </div>
+    </nav>
 
-        <!-- special Blade directive that specifies where all child pages that extend the layout can inject their own content -->
-        @yield('content')
+    @yield('content')
 
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
     </body>
 
 </html>
